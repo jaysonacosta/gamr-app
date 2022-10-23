da908e3543b5dd52123b53bb559b60ffb3c8f3d0
Jayson Acosta <36345453+jaysonacosta@users.noreply.github.com>
2022-10-23 01:56:58 -0400

    styling changes

--- src/components/Layout/Navbar/component.tsx
+++ src/components/Layout/Navbar/component.tsx
@@ -8,20 +8,22 @@ const Navbar: React.FC = () => {
   const buttonStyles = "rounded-sm border border-inherit p-2";
 
   return (
-    <nav className="flex items-center justify-between bg-slate-800 p-4 text-white">
-      <span className="font-extrabold">Gamr.app</span>
+    <nav className="bg-gray-700 p-2 text-neutral-100">
+      <div className="container mx-auto flex items-center justify-between">
+        <span className="font-extrabold">Gamr.app</span>
 
-      {session && (
-        <button className={buttonStyles} onClick={() => signOut()}>
-          Sign Out
-        </button>
-      )}
+        {session && (
+          <button className={buttonStyles} onClick={() => signOut()}>
+            Sign Out
+          </button>
+        )}
 
-      {!session && (
-        <button className={buttonStyles} onClick={() => signIn("discord")}>
-          Sign In
-        </button>
-      )}
+        {!session && (
+          <button className={buttonStyles} onClick={() => signIn("discord")}>
+            Sign In
+          </button>
+        )}
+      </div>
     </nav>
   );
 };
