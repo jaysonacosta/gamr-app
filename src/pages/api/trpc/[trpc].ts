48f0b811fe456c6f651f93be9162bc9752c25d9e
Jayson Acosta <36345453+jaysonacosta@users.noreply.github.com>
2022-10-24 18:21:43 -0400

    added Husky

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 // src/pages/api/trpc/[trpc].ts
 import { createNextApiHandler } from "@trpc/server/adapters/next";
 import { appRouter } from "../../../server/trpc/router/_app";
