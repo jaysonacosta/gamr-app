d2dcc0aeccb1e981bd3d1fa2c64f3a439b420be6
Jayson Acosta <36345453+jaysonacosta@users.noreply.github.com>
2022-10-25 14:40:25 -0400

    added message to user schema and created trpc route for message

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,9 +1,11 @@
 // src/server/trpc/router/_app.ts
 import { router } from "../trpc";
 import { authRouter } from "./auth";
+import { messageRouter } from "./message";
 
 export const appRouter = router({
   auth: authRouter,
+  message: messageRouter,
 });
 
 // export type definition of API
---
+++ src/server/trpc/router/message.ts
@@ -0,0 +1,33 @@
+import { z } from "zod";
+import { router, protectedProcedure } from "../trpc";
+
+export const messageRouter = router({
+  updateMessage: protectedProcedure
+    .input(z.object({ text: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const { text } = input;
+
+      const messageUpdate = await ctx.prisma.user.update({
+        where: {
+          id: ctx.session.user.id,
+        },
+        data: {
+          message: text,
+        },
+      });
+
+      return messageUpdate;
+    }),
+  getMessage: protectedProcedure.query(async ({ ctx }) => {
+    const user = await ctx.prisma.user.findUnique({
+      where: {
+        id: ctx.session.user.id,
+      },
+    });
+
+    let message;
+    user ? (message = user.message) : null;
+
+    return message;
+  }),
+});
